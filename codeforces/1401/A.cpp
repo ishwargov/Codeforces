81387de41955f0191b062e60e9b34a3d1b05dd10
ishwar <ishwar.govind@gmail.com>
2020-08-31 23:11:00 +0530

    Add solution for problem `A - Distance and Axis` Link: https://codeforces.com/contest/1401/problem/A Tags: constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1401/submission/91514598

---
+++ codeforces/1401/A.cpp
@@ -0,0 +1,45 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=k;i<=n;i++)
+#define repi(i,k,n) for(ll i=k;i>=n;i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    ll n,k;
+    cin>>n>>k;
+    if(k>n){
+        cout<<k-n;
+    }
+    else{
+        if((n-k)%2==0){
+            cout<<"0";
+        }
+        else{
+            cout<<"1";
+        }
+    }
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int T;
+    cin>>T;
+    rep(t,1,T){
+    //    cout<<"Case #"<<t<<": ";
+        solve();
+        cout<<"\n";
+    }
+    //timetaken();
+    return 0;
+}

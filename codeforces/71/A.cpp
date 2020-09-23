f363afc49350c85f20b071343dfd8727a07d0d34
ishwar <ishwar.govind@gmail.com>
2020-09-23 23:33:00 +0530

    Add solution for problem `A - Way Too Long Words` Link: https://codeforces.com/contest/71/problem/A Tags: strings, *800 Ref: https://codeforces.com/contest/71/submission/93587435

---
+++ codeforces/71/A.cpp
@@ -0,0 +1,42 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=k;i<=n;i++)
+#define repi(i,k,n) for(ll i=k;i>=n;i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    string s,ans;
+    cin>>s;
+    if(s.size()>10){    
+        ans = string(1,s[0]) + to_string((s.size()-2)) + string(1,s[s.size()-1]);  
+    }
+    else{
+        ans = s;
+    }
+    cout<<ans;
+
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int T;
+    cin>>T;
+    rep(t,1,T){
+    //    cout<<"Case #"<<t<<": ";
+        solve();
+        cout<<"\n";
+    }
+    //timetaken();
+    return 0;
+} 

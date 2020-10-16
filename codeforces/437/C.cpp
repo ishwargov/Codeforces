e50fcec865fd2ad1db99a9a614a79d5e3b654e8b
ishwar <ishwar.govind@gmail.com>
2020-10-16 10:35:00 +0530

    Add solution for problem `C - The Child and Toy` Link: https://codeforces.com/contest/437/problem/C Tags: graphs, greedy, sortings, *1400 Ref: https://codeforces.com/contest/437/submission/95633252

---
+++ codeforces/437/C.cpp
@@ -0,0 +1,39 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=k;i<=n;i++)
+#define repi(i,k,n) for(ll i=k;i>=n;i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+
+void solve(){
+    ll n,k,x,y,ans=0;
+    cin>>n>>k;
+    ll cost[1010];
+    rep(i,1,n){
+        cin>>x;
+        cost[i] = x;
+    }
+    rep(i,0,k-1){
+        cin>>x>>y;
+        ans += min(cost[x],cost[y]);
+    }
+    cout<<ans;
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    solve();
+    //timetaken();
+    return 0;
+} 

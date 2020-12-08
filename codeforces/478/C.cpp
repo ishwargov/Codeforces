64a335d19319b836ba6d60dd1dc784bbbad78d3d
ishwar <ishwar.govind@gmail.com>
2020-12-08 11:48:00 +0530

    Add solution for problem `C - Table Decorations` Link: https://codeforces.com/contest/478/problem/C Tags: greedy, *1800 Ref: https://codeforces.com/contest/478/submission/100670248

---
+++ codeforces/478/C.cpp
@@ -0,0 +1,31 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=k;i<=n;i++)
+#define repi(i,k,n) for(ll i=k;i>=n;i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    ll a[3];
+    cin>>a[0]>>a[1]>>a[2];
+    sort(a,a+3);
+    if(a[2]>=2*(a[0]+a[1])) cout<<a[0]+a[1];
+    else cout<<(a[0]+a[1]+a[2])/3;
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    solve();
+    //timetaken();
+    return 0;
+} 

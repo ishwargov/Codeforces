fe98d0fe35c55092ce29a7875ed39abb7430682f
ishwar <ishwar.govind@gmail.com>
2020-12-20 09:18:00 +0530

    Add solution for problem `A - Red-Blue Shuffle` Link: https://codeforces.com/contest/1459/problem/A Tags: math, probabilities, *800 Ref: https://codeforces.com/contest/1459/submission/101815160

---
+++ codeforces/1459/A.cpp
@@ -0,0 +1,54 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=k;i<=n;i++)
+#define repi(i,k,n) for(ll i=k;i>=n;i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll rs=0,ls=0;
+    string r,b;
+    cin>>r;
+    cin>>b;
+
+    rep(i,0,n-1){
+        if(r[i]>b[i]) rs++;
+        else if(r[i]==b[i]){ ls++;rs++; }
+        else ls++;
+    }
+    if(rs>ls){
+        cout<<"RED";
+    }
+    else if(ls>rs){
+        cout<<"BLUE";
+    }
+    else{
+        cout<<"EQUAL";
+    }
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int T;
+    cin>>T;
+    rep(t,1,T){
+    //    cout<<"Case #"<<t<<": ";
+        solve();
+        cout<<"\n";
+    }
+    //timetaken();
+    return 0;
+} 

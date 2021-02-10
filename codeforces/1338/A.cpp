d4e8ff2070ee2dc5e52423b94afa44c625a04b85
ishwar <ishwar.govind@gmail.com>
2021-02-10 21:21:00 +0530

    Add solution for problem `A - Powered Addition` Link: https://codeforces.com/contest/1338/problem/A Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1338/submission/107073166

---
+++ codeforces/1338/A.cpp
@@ -0,0 +1,48 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=(k);i<=(n);i++)
+#define repi(i,k,n) for(ll i=(k);i>=(n);i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector <ll> a(n);    
+    rep(i,0,n-1){
+        cin>>a[i];
+    }
+    ll curmx = a[0];
+    ll ans = 0;
+    rep(i,1,n-1){
+        ll dif = curmx-a[i],cnt=0;
+        if(a[i]>curmx){curmx = a[i];}
+        while(dif>0){dif/=2;cnt++;}
+        ans = max(cnt,ans);
+    }
+    cout<<ans;
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int T;
+    cin>>T;
+    rep(t,1,T){
+    //    cout<<"Case #"<<t<<": ";
+        solve();
+        cout<<"\n";
+    }
+    //timetaken();
+    return 0;
+} 

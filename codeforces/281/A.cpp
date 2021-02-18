1664baeebb8fdcc937b7fbbbc0a09f19b841c6c9
ishwar <ishwar.govind@gmail.com>
2021-02-18 13:35:00 +0530

    Add solution for problem `A - Word Capitalization` Link: https://codeforces.com/contest/281/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/281/submission/107754700

---
+++ codeforces/281/A.cpp
@@ -0,0 +1,34 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=(k);i<=(n);i++)
+#define repi(i,k,n) for(ll i=(k);i>=(n);i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    string s;
+    cin>>s;
+    if(s[0]<'a'){cout<<s;}
+    else{
+        cout<<(char)(s[0]-'a'+'A');
+        cout<<s.substr(1,s.size()-1);
+    }
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    solve();
+    //timetaken();
+    return 0;
+} 

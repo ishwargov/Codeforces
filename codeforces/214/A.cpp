270f0ae414ef97b7cae55248a36c69f67b90ca26
ishwar <ishwar.govind@gmail.com>
2021-02-23 11:56:00 +0530

    Add solution for problem `A - System of Equations` Link: https://codeforces.com/contest/214/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/214/submission/108228044

---
+++ codeforces/214/A.cpp
@@ -0,0 +1,35 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=(k);i<=(n);i++)
+#define repi(i,k,n) for(ll i=(k);i>=(n);i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    ll n,m,ans=0;
+    cin>>n>>m;
+    rep(i,0,1000){
+        rep(j,0,1000){
+            ans += (i*i+j==n&&j*j+i==m);
+        }
+    }
+    cout<<ans;
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    solve();
+    //timetaken();
+    return 0;
+} 

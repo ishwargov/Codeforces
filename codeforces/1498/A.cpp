ae924ccce00697199d54f2b0658fded4fa2ebca9
ishwar <ishwar.govind@gmail.com>
2021-03-30 14:06:00 +0530

    Add solution for problem `A - GCD Sum` Link: https://codeforces.com/contest/1498/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1498/submission/111451964

---
+++ codeforces/1498/A.cpp
@@ -0,0 +1,50 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=(k);i<=(n);i++)
+#define repi(i,k,n) for(ll i=(k);i>=(n);i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+bool poss(ll a){
+    ll s = a,b=0;
+    while(s>0){
+        b += s%10;
+        s/=10;
+    }
+    if(__gcd(a,b)>1) return true;
+    else return false;
+}
+
+void solve(){
+    ll n,a;
+    cin>>n;
+    a = n;
+    while(!poss(a)){
+        a++;
+    }
+    cout<<a;
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int T;
+    cin>>T;
+    rep(t,1,T){
+    //    cout<<"Case #"<<t<<": ";
+        solve();
+        cout<<"\n";
+    }
+    //timetaken();
+    return 0;
+} 

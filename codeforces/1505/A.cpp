c4b17ebef9775339fd20e81d19403b5cfbf63228
ishwar <ishwar.govind@gmail.com>
2021-04-11 23:24:00 +0530

    Add solution for problem `A - Is it rated - 2` Link: https://codeforces.com/contest/1505/problem/A Tags: implementation, interactive, *900 Ref: https://codeforces.com/contest/1505/submission/112716830

---
+++ codeforces/1505/A.cpp
@@ -0,0 +1,31 @@
+/* XxSupremeLordxX Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=(k);i<=(n);i++)
+#define repi(i,k,n) for(ll i=(k);i>=(n);i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    string s;
+    while(getline(cin,s)){
+        if(cin){
+            cout<<"NO\n";
+        }
+    }
+}
+
+int main(){   
+    solve();
+    //timetaken();
+    return 0;
+} 

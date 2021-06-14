f28f168afdec1706f0d7150cbcd5fa30358d2a4f
ishwar <ishwar.govind@gmail.com>
2021-06-14 16:26:00 +0530

    Add solution for problem `B - Histogram Ugliness` Link: https://codeforces.com/contest/1534/problem/B Tags: greedy, implementation, math, *1100 Ref: https://codeforces.com/contest/1534/submission/119444233

---
+++ codeforces/1534/B.cpp
@@ -0,0 +1,47 @@
+/* SupremeLord Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=(k);i<=(n);i++)
+#define repi(i,k,n) for(ll i=(k);i>=(n);i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (float)(clock() - begtime)/1000.0
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector<ll> a(n+2,0);
+    rep(i,1,n){
+        cin>>a[i];
+    }
+    ll ans = 0,cost=a[1];
+    rep(i,1,n){
+        cost +=abs(a[i+1]-a[i]);
+        if(a[i]>a[i-1]&&a[i]>a[i+1]){
+            ans += a[i] - max(a[i+1],a[i-1]);
+        }
+    }
+    cout<<cost-ans;
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int T;
+    cin>>T;
+    rep(t,1,T){
+    //    cout<<"Case #"<<t<<": ";
+        solve();
+        cout<<"\n";
+    }
+    //timetaken();
+    return 0;
+} 

5a0162952fcce466e993d196527cfa8a7e00d3c5
ishwar <ishwar.govind@gmail.com>
2021-08-19 20:13:00 +0530

    Add solution for problem `F2 - Nearest Beautiful Number (hard version)` Link: https://codeforces.com/contest/1560/problem/F2 Tags: bitmasks, brute force, constructive algorithms, dfs and similar, dp, greedy, *2100 Ref: https://codeforces.com/contest/1560/submission/126457035

---
+++ codeforces/1560/F2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string n;
+    int k;
+    cin>>n>>k;
+
+    while (1){
+        set<char> s;
+        for (auto c : n) s.insert(c);
+        if (s.size() <= k) {cout<<n;return;}
+
+        s.clear();
+        int j = 0;
+        while(1){
+            s.insert(n[j]);
+            if (s.size() > k){
+            while (n[j] == '9') j--;
+            n[j]++;
+            for (int i=j+1; i<n.size(); i++) n[i] = '0';
+            break;
+        }
+            j++;
+        //cout<<n<<"\n";
+        }
+    }
+    cout<<n;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        solve(); 
+        cout<<'\n';
+    }
+    return 0;
+}

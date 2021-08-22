1936dc9ae81c04ded04cada5c68beac3a1821604
ishwar <ishwar.govind@gmail.com>
2021-08-22 08:37:00 +0530

    Add solution for problem `A - Cancel the Trains` Link: https://codeforces.com/contest/1453/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1453/submission/126661603

---
+++ codeforces/1453/A.cpp
@@ -0,0 +1,49 @@
+/* SupremeLord Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=(k);i<=(n);i++)
+#define repi(i,k,n) for(ll i=(k);i>=(n);i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (double)(clock() - begtime)/CLOCKS_PER_SEC
+
+void solve(){
+    int ans = 0,n,m;
+    vector <ll> a(101,0);
+    cin>>n>>m;
+    rep(i,0,n-1){
+        ll x;
+        cin>>x;
+        a[x]++;
+    }
+    rep(i,0,m-1){
+        ll x;
+        cin>>x;
+        if(a[x]>=1){
+            ans++;
+        }
+    }
+    cout<<ans;
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int T;
+    cin>>T;
+    rep(t,1,T){
+    //    cout<<"Case #"<<t<<": ";
+        solve();
+        cout<<"\n";
+    }
+    //timetaken();
+    return 0;
+} 

b3b1100b0ccf1675139b3a2545f79c3ac5e96a74
ishwar <ishwar.govind@gmail.com>
2021-09-09 11:48:00 +0530

    Add solution for problem `A - Balanced Substring` Link: https://codeforces.com/contest/1569/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1569/submission/128317327

---
+++ codeforces/1569/A.cpp
@@ -0,0 +1,44 @@
+/* SupremeLord Ishwar Govind */
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define PI acos(-1)
+#define ll long long int
+#define fi first    
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ub upper_bound
+#define lb lower_bound
+#define rep(i,k,n) for(ll i=(k);i<=(n);i++)
+#define repi(i,k,n) for(ll i=(k);i>=(n);i--)
+clock_t begtime = clock();
+#define timetaken() cout << "\n\nTime elapsed: " << (double)(clock() - begtime)/CLOCKS_PER_SEC
+
+void solve(){
+    string s;
+    int n;
+    cin>>n;
+    cin>>s;
+    for(int i=0;i<n-1;i++){
+        if(s[i]!=s[i+1]){
+            cout<<i+1<<" "<<i+2;
+            return;
+        }
+    }
+    cout<<"-1 -1";
+}
+
+int main(){   
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int T;
+    cin>>T;
+    rep(t,1,T){
+    //    cout<<"Case #"<<t<<": ";
+        solve();
+        cout<<"\n";
+    }
+    //timetaken();
+    return 0;
+} 
